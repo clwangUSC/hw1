e16017f5f09aa3fd78acb93930109bcf56736450
Christina Wang <clwang@usc.edu>
2023-01-16 23:54:45 +0000

    part 1 done

--- split.cpp
+++ split.cpp
@@ -11,6 +11,8 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include<iostream>
+using namespace std;
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +20,43 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+    if(in == NULL || in==nullptr)
+        return;
+    Node* temp =in;
+    
+    if(in->value%2==0)
+    {
+        if(evens == NULL || evens ==nullptr)
+        {
+            evens=temp;
+            in=in->next;
+            evens->next=NULL;
+            split(in,odds,evens->next);
+        }
+        else
+        {
+          return;
+        }
+        
+    }
+    else
+    {
+        if(odds == NULL || odds ==nullptr)
+        {
+            odds=temp;
+            in=in->next;
+            odds->next=NULL;
+            split(in,odds->next,evens);
+            
+        }
+        else
+        {
+          return;
+        }
+        
+    }
+    
+
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,47 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include<iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+
+  Node* n6 = new Node(6, NULL);
+  Node* n5 = new Node(5, n6);
+  Node* n4 = new Node(4, n5);
+  Node* n3 = new Node(3, n4);
+  Node* n2 = new Node(2, n3);
+  Node* n = new Node(1, n2);
+
+
+
+  Node* eve = NULL;
+  Node* odd = NULL;
+
+  split(n,odd,eve);
+
+  Node* temp;
+  while(odd->next != NULL && odd->next !=nullptr)
+  {
+    temp = odd;
+    cout<<"delete "<<odd->value<<endl;
+    odd=odd->next;
+    delete temp;
+  }
+    temp = odd;
+    cout<<"delete "<<odd->value<<endl;
+    delete temp;
+
+  while(eve->next != NULL && eve->next !=nullptr)
+  {
+    temp = eve;
+    cout<<"delete even "<<eve->value<<endl;
+    eve=eve->next;
+    delete temp;
+  }
+    temp = eve;
+    cout<<"delete even "<<eve->value<<endl;
+    delete temp;
 
 }
